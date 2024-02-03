e9a26cb21d7820e94e8318dbba4a0fd7d22d024a
kkkieuly <lyykieua@gmail.com>
2024-02-03 02:38:54 +0100

    spawn
    not tested and didnt fix error yet

--- Ghost Mansion/Assets/Assets/Scripts/GhostBehaviour.cs
+++ Ghost Mansion/Assets/Assets/Scripts/GhostBehaviour.cs
@@ -1,17 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+
 
 public class GhostBehaviour : MonoBehaviour
 {
-    public GameObject ghost;
+    GameObject ghost;
     
 
     // Start is called before the first frame update
     void Start()
     {
         disappear();
-        randomStartposition();
+        //randomStartposition();
     }
 
     // Update is called once per frame
@@ -21,9 +24,27 @@ public class GhostBehaviour : MonoBehaviour
     }
 
 
-    public void randomStartposition()
+    /* public void randomStartposition()
     {
         Vector3 randomPosition = new Vector3(Random.Range(-20, 20), Random.Range(-5, 20), Random.Range(2, 6));
+    } */
+
+    public void randomSpawnPosition()
+    {
+        Vector3[] spawns = new Vector3[5];
+
+        spawns[0] = new Vector3(-10.85048f, -1.865619f, 6.553421f);
+        spawns[1] = new Vector3(-9.792572f, -0.2184101f, 9.876396f);
+        spawns[2] = new Vector3(1.442768f, 2.088465f, 19.90268f);
+        spawns[4] = new Vector3(14.74884f, -0.003983911f, 16.1106f);
+        spawns[5] = new Vector3(-18.97742f, 0.5934364f, 20.27015f);
+
+
+        int rs = new Random.Range(0, spawns.Length);
+
+        ghost.SetPosition(spawns[rs]);
+
+
     }
 
     public void disappear()
@@ -39,5 +60,10 @@ public class GhostBehaviour : MonoBehaviour
     public Vector3 getPosition()
     {
         return this.transform.position;
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        this.transform.position = position;
     }
 }
