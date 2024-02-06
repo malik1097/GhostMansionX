32d691f4b7376de445f0e1d6b12efe7d00fb3c67
kkkieuly <lyykieua@gmail.com>
2024-02-06 20:52:42 +0100

    Update InteractObject.cs

--- Ghost Mansion/Assets/InteractObject.cs
+++ Ghost Mansion/Assets/InteractObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class InteractObject : MonoBehaviour
 {
+    public LineRenderer lineRenderer;
+    public OVRInput.Button b;
+    public float maxRange;
+    public string interactTag;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,24 @@ public class InteractObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxRange))
+        {
+            lineRenderer.SetPosition(1, Vector3.forward * hit.distance);
+
+
+            if (OVRInput.Get(b))
+            {
+                if (hit.transform.tag.Contains(interactTag))
+                {
+                    Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+                    //rb.
+                }
+            }
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, Vector3.forward * maxRange);
+        }
     }
 }
