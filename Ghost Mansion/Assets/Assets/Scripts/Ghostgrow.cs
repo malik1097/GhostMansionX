31ceaee4583bb83a3216089e6d7286341cfabfeb
Olbu2181 <oliver.buco@study.hs-duesseldorf.de>
2024-02-06 14:15:54 +0100

    ghostgrow script

--- Ghost Mansion/Assets/Assets/Scripts/Ghostgrow.cs
+++ Ghost Mansion/Assets/Assets/Scripts/Ghostgrow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Testghostbehaviour : MonoBehaviour
+public class Ghostgrow : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
